c23e87e786fdfe2c7374309fac384a7626b3f387
vikash-mahar <vmahar808@gmail.com>
2025-07-29 20:16:01 +0530

    minor bug fixes

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,8 @@ import cookieParser from 'cookie-parser'
 const app=express()
 
 app.use(cors({
-    origin:["http://localhost:3000"],
+    // origin:["http://localhost:3000"],
+    origin:["https://mobileauth.onrender.com"],
     credentials:true
 }))
 app.use(cookieParser())
